a51e79b71c07aae1d16e2c63daa23ce3331646f4
DHANUSH MD <dhanushmd0106@gmail.com>
2024-02-02 15:41:27 +0530

    added comments for .js file

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
 
 function performTask() {
+    // Check if the input box is empty
     if (inputBox.value === '') {
         alert("Please enter a task");
     } else {
+        // Create a new list item
         const listItem = document.createElement("li");
+
+        // Set the inner HTML of the list item to the value of the input box
         listItem.innerHTML = inputBox.value;
+
+        // Append the list item to the list container
         listContainer.appendChild(listItem);
 
-
-
+        // Create a span element for the delete button
         const span = document.createElement("span");
+
+        // Set the inner HTML of the span element to the delete icon 
         span.innerHTML = "\u00d7";
+
+        // Append the span element to the list item
         listItem.appendChild(span);
 
-
     }
+    // Clear the input box
     inputBox.value = '';
     dataSave()
 }
@@ -32,10 +41,15 @@ listContainer.addEventListener("click", function (e) {
     }
 }, false);
 
+
+
+// Function to load the data from local storage
 function dataSave() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
+
+// Function to load the data from local storage
 function dataLoad() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
